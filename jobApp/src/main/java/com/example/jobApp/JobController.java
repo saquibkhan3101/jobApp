fc0a89413ceb73d0c52e7909b2003c81fa86258d
saquib <saquibkhan3101@gmail.com>
2026-05-17 18:51:09 +0530

    fixed JobController handleForm to bind JobPost and pass data to success page

--- jobApp/src/main/java/com/example/jobApp/JobController.java
+++ jobApp/src/main/java/com/example/jobApp/JobController.java
@@ -1,12 +1,28 @@
 package com.example.jobApp;
 
+import com.example.jobApp.model.JobPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
-    @RequestMapping({"/","home"})
-    public  String home(){
+
+    @RequestMapping({"/", "home"})
+    public String home() {
         return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addjob() {
+        return "addjob";
+    }
+
+    @RequestMapping("/handleForm")
+    public String handleForm(@ModelAttribute JobPost jobPost, Model model) {
+
+        model.addAttribute("jobPost", jobPost);
+        return "success";
     }
 }
