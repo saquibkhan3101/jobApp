8542ad9919cab9bcab97f5cf0fc68b3e4bf2024b
saquib <saquibkhan3101@gmail.com>
2026-05-17 15:12:12 +0530

    store changes

---
+++ jobApp/src/test/java/com/example/jobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
